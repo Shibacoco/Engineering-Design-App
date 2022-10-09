cd6c734f95b27a039eea580d9d6fa6424d810e22
MikTheMilkMan <82846573+MikTheMilkMan@users.noreply.github.com>
2022-10-09 23:09:18 +0200

    ingredients show up in table

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+const electron = require('electron')
+
+// Enable live reload for Electron too
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -41,3 +49,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+

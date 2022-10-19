b2bfc4e601d0a6dc52a9a7195762e927c5d9dae4
MikTheMilkMan <82846573+MikTheMilkMan@users.noreply.github.com>
2022-10-19 17:20:13 +0200

    show added ingredients, some eventlisteners, removed some things from html

--- renderer.js
+++ renderer.js
@@ -10,28 +10,37 @@ const ownedFood = [
     // {
     //     "id": "0",
     //     "name": "tomato",
-    //     "expiration date": "01-01-1970",
     //     "category": "fruit",
+    //     "expiration date": "01-01-1970",
     //     "amount": "2"
     // },
     // {
     //     "id": "1",
     //     "name": "carrot",
-    //     "expiration date": "01-01-1970",
     //     "category": "vegetables",
+    //     "expiration date": "01-01-1970",
     //     "amount": "5"
     // },
     // {
     //     "id": "2",
     //     "name": "banana",
-    //     "expiration date": "01-01-1970",
     //     "category": "fruit",
-    //     "amount": "69"
-    //
+    //     "expiration date": "01-01-1970",
+    //     "amount": "69",
+    // }
 ];
+
+const barcodes = [
+    {
+        "barcode": 1,
+        "name": "tomato",
+        "category": "fruit",
+        "daysUntilExpiration": 5,
+    }
+]
 const infoTable = document.getElementById("foodTable");
 const removeButton = document.getElementById("deleteSelected");
-const manualAddReveal = document.getElementById("manualAddReveal");
+const addFood = document.getElementById("addFood");
 
 
 //-------------------------------------------------CLASSES--------------------------------------------------------------
@@ -59,68 +68,141 @@ class food {
 
 //------------------------------------------------FUNCTIONS-------------------------------------------------------------
 
-function showIngredients(e){
+function insertIngredient(food) {
+    const row = infoTable.insertRow();
+
+    const cell0 = row.insertCell(0);
+    const cell1 = row.insertCell(1);
+    const cell2 = row.insertCell(2);
+    const cell3 = row.insertCell(3);
+    const cell4 = row.insertCell(4);
+    const cell5 = row.insertCell(5);
+
+    cell0.innerHTML = '<input name="checkboxInTable" type="checkbox" class="checkboxtable">'
+    cell1.innerHTML = '<td class="tabledata">' + food["id"] + '</td>';
+    cell2.innerHTML = '<td class="tabledata">' + food["name"] + '</td>';
+    cell4.innerHTML = '<td class="tabledata">' + food["category"] + '</td>';
+    cell3.innerHTML = '<td class="tabledata">' + food["expirationDate"] + '</td>';
+    cell5.innerHTML = '<td class="tabledata">' + food["amount"] + '</td>';
+}
+
+
+function showIngredients(e) {
     e.forEach(e => {
-        const row = infoTable.insertRow();
-
-        const cell0 = row.insertCell(0);
-        const cell1 = row.insertCell(1);
-        const cell2 = row.insertCell(2);
-        const cell3 = row.insertCell(3);
-        const cell4 = row.insertCell(4);
-        const cell5 = row.insertCell(5);
-
-        cell0.innerHTML = '<input name="checkboxInTable" type="checkbox" class="checkboxtable">'
-        cell1.innerHTML = '<td class="tabledata">' + e["id"] + '</td>';
-        cell2.innerHTML = '<td class="tabledata">' + e["name"] + '</td>';
-        cell3.innerHTML = '<td class="tabledata">' + e["expiration date"] + '</td>';
-        cell4.innerHTML = '<td class="tabledata">' + e["category"] + '</td>';
-        cell5.innerHTML = '<td class="tabledata">' + e["amount"] + '</td>';
+        insertIngredient(e);
     })
+
     return null;
 }
+
+function showNewestIngredient(e) {
+    insertIngredient(e.at(e.length - 1))
+    console.log(e.at(e.length - 1));
+    console.log(ownedFood);
+}
+
+function showForm(manual, div) {
+    if (manual) {
+
+        div.innerHTML = "<form id=\"manualAddForm\" class=\"form\">" +
+            "  <p class=\"bold\">Add your ingredient here</p>\n" +
+            "  <input id=\"manualName\" type=\"text\" placeholder=\"Name\">\n" +
+            "  <input id=\"manualCategory\" type=\"text\" placeholder=\"Category\">\n" +
+            "  <input id=\"manualAmount\" type=\"number\" placeholder=\"0\">\n" +
+            "  <input id=\"manualExpirationDate\" type=\"date\" class=\"date-input\">\n" +
+            "  <input id=\"manualAdd\" type=\"button\" value=\"Add Ingredient to list\">\n" +
+            "</form>\n";
+    }
+    else if (!manual)
+    {
+        div.innerHTML = "<form id=\"manualAddForm\" class=\"form\">" +
+            "  <input id=\"activateSanner\" type=\"button\" value=\"Activate scanner\">\n" +
+            "  <p class=\"bold\">Add your ingredient here</p>\n" +
+            "  <input id=\"manualName\" type=\"text\" placeholder=\"Name\">\n" +
+            "  <input id=\"manualCategory\" type=\"text\" placeholder=\"Category\">\n" +
+            "  <input id=\"manualAmount\" type=\"number\" placeholder=\"0\">\n" +
+            "  <input id=\"manualExpirationDate\" type=\"date\" class=\"date-input\">\n" +
+            "  <input id=\"manualAdd\" type=\"button\" value=\"Add Ingredient to list\">\n" +
+            "</form>\n";
+    }
+
+}
+
+function addToOwnedFoodsManually(div) {
+    const manualAdd = document.getElementById('manualAdd');
+    manualAdd.addEventListener('click', (() => {
+        let name = document.getElementById('manualName').value;
+        let category = document.getElementById('manualCategory').value;
+        let amount = document.getElementById('manualAmount').value;
+        let date = document.getElementById('manualExpirationDate').value;
+
+        //check if empty
+
+        // console.log("---------------------");
+        new food(name, category, amount, date);
+        div.innerHTML = "";
+        showNewestIngredient(ownedFood);
+    }))
+}
+
+function addToOwnedFoodsScanner(){
+    const activateScanner = document.getElementById('activateScanner');
+    activateScanner.addEventListener('click', (()=> {
+        //activate scanner and wait for input
+    }))
+}
+
 
 //-------------------------------------------EVENTLISTENERS-------------------------------------------------------------
 
 removeButton.addEventListener('click', (() => {
     const checkboxes = document.getElementsByName("checkboxInTable");
 
-    if(document.getElementById("selectAllCheckbox").checked){
+    if (document.getElementById("selectAllCheckbox").checked) {
         document.getElementById("selectAllCheckbox").checked = false;
     }
 
-    for(let i = checkboxes.length; i > 0; i--) {
-        if(checkboxes[i - 1].checked){
+    for (let i = checkboxes.length; i > 0; i--) {
+        if (checkboxes[i - 1].checked) {
             ownedFood.splice(i - 1, 1)
             checkboxes[i - 1].parentElement.parentElement.remove();
         }
     }
 }))
 
-manualAddReveal.addEventListener('click', (() => {
-    const form = document.getElementById("manualAddForm");
-    form.innerHTML = "<p class=\"bold\">Add your ingredient here</p>\n" +
-        "  <input id=\"manualName\" type=\"text\" placeholder=\"Name\">\n" +
-        "  <input id=\"manualCategory\" type=\"text\" placeholder=\"Category\">\n" +
-        "  <input id=\"manualAmount\" type=\"number\" placeholder=\"0\">\n" +
-        "  <input id=\"manualExpirationDate\" type=\"date\" class=\"date-input\">\n" +
-        "  <input id=\"manualAdd\" type=\"button\" value=\"Add Ingredient to list\">";
+addFood.addEventListener('click', (() => {
+    const addFoodDiv = document.getElementById('addFoodChoice');
+    addFoodDiv.innerHTML =
+        "<div class=\"center\">\n" +
+        "   <input type=\"button\" id=\"manually\" value=\"Add Manually\">\n" +
+        "   <input type=\"button\" id=\"scanner\" value=\"Add by scanning\">\n" +
+        "</div>\n";
+
+    const manually = document.getElementById('manually');
+    const scanner = document.getElementById('scanner');
 
 
-    const manualAdd = document.getElementById('manualAdd');
-    manualAdd.addEventListener('click', (() => {
-        let name = document.getElementById('manualName');
-        let category = document.getElementById('manualCategory');
-        let amount = document.getElementById('manualAmount');
-        let date = document.getElementById('manualExpirationDate');
+    manually.addEventListener('click', () => {
+        showForm(true, addFoodDiv);
+        addToOwnedFoodsManually(addFoodDiv);
+    })
 
-        ownedFood.push(new food(name, category, date, amount));
 
-        //update window to show new entry
-    }))
+    scanner.addEventListener('click', () => {
+
+        showForm(false, addFoodDiv);
+        addToOwnedFoodsScanner(addFoodDiv);
+
+
+        //deze moet dynamisch worden aangepast naar de output die door de scanner gegenereerd wordt
+        let barcode = 1;
+        let tempDate = new Date();
+
+        let thing = barcodes.find(element => element.barcode === barcode);
+
+        // addToOwnedFoods();
+    })
 }));
-
-
 
 
 //----------------------------------------SHOW INITIAL INGREDIENTS------------------------------------------------------
@@ -132,6 +214,7 @@ window.onload = showIngredients(ownedFood);
 const testbutton = document.getElementById("testbutton");
 
 testbutton.addEventListener('click', () => {
+    console.log(ownedFood);
 });
 
 

d70e0e0e5df82b3cf75ac5c4626fb700f6bfb5cf
MikTheMilkMan <82846573+MikTheMilkMan@users.noreply.github.com>
2022-10-24 16:29:33 +0200

    changed to be better

--- preload.js
+++ preload.js
@@ -5,13 +5,16 @@
  * 
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+      
+    }
+  })
+
+
+//https://stackoverflow.com/questions/1098040/checking-if-a-key-exists-in-a-javascript-object
+//var obj = { key: undefined };
+//console.log("key" in obj); // true, regardless of the actual value
